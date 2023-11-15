affcab3d77dae858043dbe744e5e3179c86c981c
Nick Sharpe <nickdsharpe@comcast.net>
2023-11-15 14:22:41 -0700

    Work

--- .ipynb_checkpoints/get_stats-checkpoint.py
+++ .ipynb_checkpoints/get_stats-checkpoint.py
@@ -27,18 +27,25 @@ def get_stats(team, player, game):
     off_PPP = offense_file['ovr_data']['data']
     off_PPP = pd.DataFrame(off_PPP).transpose()
     off_PPP = PPP(off_PPP)
+    try:
+        off_rim_PPP = offense_file['Rim']['data']
+        off_rim_PPP = pd.DataFrame(off_rim_PPP).transpose()
+        off_rim_PPP = PPP(off_rim_PPP)
+    except:
+        offense_rim_PPP = 'N/A'
+        off_rim_TS = 'N/A'
+        off_rim_SQ = 'N/A'
 
-    off_rim_PPP = offense_file['Rim']['data']
-    off_rim_PPP = pd.DataFrame(off_rim_PPP).transpose()
-    off_rim_PPP = PPP(off_rim_PPP)
-    
     def_PPP = defense_file['ovr_data']['data']
     def_PPP = pd.DataFrame(def_PPP).transpose()
     def_PPP = defense_PPP(def_PPP)
-
-    def_rim_PPP = defense_file['Rim']['data']
-    def_rim_PPP = pd.DataFrame(def_rim_PPP).transpose()
-    def_rim_PPP = defense_PPP(def_rim_PPP)
+    
+    try:
+        def_rim_PPP = defense_file['Rim']['data']
+        def_rim_PPP = pd.DataFrame(def_rim_PPP).transpose()
+        def_rim_PPP = defense_PPP(def_rim_PPP)
+    except:
+        pass
     
     
     
@@ -83,18 +90,10 @@ def get_stats(team, player, game):
             nugg_off_ax.plot(x,y, marker='^', color='#2393de', markersize=9)
     
     nugg_off_ax.set_title(f'{player} Shot Chart | {game}', y=1.03)
-    nugg_off_ax.set_xlabel(f'Total PPP: {offense_PPP} \n Shooting TS%: {off_shoot_TS} on {off_total_SQ} SQ \n 3pt FG %: {off_3pt_fg_per} on {off_3pt_SQ} SQ')
-    print(f'{player} Shot Chart | {game}')
-    print('-----------------------------------------')
-    print(f'Total PPP: {offense_PPP}')
-    print(f'Shooting TS%: {off_shoot_TS} on {off_total_SQ} SQ')
-    print(f'3pt FG %: {off_3pt_fg_per} on {off_3pt_SQ} SQ\n')
-
-    print(f'Total FTR: {off_FTR}\n')
-
-    print(f'Rim PPP: {offense_rim_PPP}')
-    print(f'Rim TS%: {off_rim_TS} on {off_rim_SQ} SQ')
+    nugg_off_ax.set_xlabel(f'Total PPP:  {offense_PPP} \n Shooting TS%:  {off_shoot_TS} on {off_total_SQ} SQ \n 3pt FG %:  {off_3pt_fg_per} on {off_3pt_SQ} SQ \n \n Total FTR:  {off_FTR} \n \n Rim PPP:  {offense_rim_PPP} \n Rim TS%:  {off_rim_TS} on {off_rim_SQ} SQ')
 
     plt.show()
+    #plt.subplots_adjust(top=0.88)
+    nugg_off_fig.savefig(f'data/{game}/{team}/Offense/{player}.png', bbox_inches='tight', dpi=nugg_off_fig.dpi)
 
     return off_PPP, off_rim_PPP, nugg_off_fig
